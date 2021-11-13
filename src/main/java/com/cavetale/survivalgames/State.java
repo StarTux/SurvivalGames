d117a54ae84c0458d51f3b5eae83bb7f401b047c
StarTux <ths.loy@googlemail.com>
2021-11-13 21:41:19 +0100

    Longer countdown

--- src/main/java/com/cavetale/survivalgames/State.java
+++ src/main/java/com/cavetale/survivalgames/State.java
@@ -2,7 +2,7 @@ package com.cavetale.survivalgames;
 
 public enum State {
     IDLE,
-    COUNTDOWN(30),
+    COUNTDOWN(60),
     LOOTING(45),
     FREE_FOR_ALL(60 * 8),
     COUNTDOWN_SUDDEN_DEATH(10),
